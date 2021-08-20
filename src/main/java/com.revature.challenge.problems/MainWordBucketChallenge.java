b977809d3cb38f99e5d0a5eff8bed5519d0a868e
ozzy castillo <oswaldo.castillo@revature.net>
2021-08-20 12:51:08 -0500

    wordbucket class

---
+++ src/main/java/com.revature.challenge.problems/MainWordBucketChallenge.java
@@ -0,0 +1,12 @@
+package com.revature.challenge.problems;
+
+public class MainWordBucketChallenge {
+
+    public static void main (String[] args){
+        int N = 10;
+        String str = "she sells sea shells by the sea";
+        WordBucket.sol(str,N);
+
+    }
+
+}
---
+++ src/main/java/com.revature.challenge.problems/WordBucket.java
@@ -0,0 +1,9 @@
+package com.revature.challenge.problems;
+
+public class WordBucket {
+
+    public static void sol(String str, int N){
+
+    }
+
+}
