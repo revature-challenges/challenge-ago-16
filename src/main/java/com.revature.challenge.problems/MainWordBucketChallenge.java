2abb7002932ae0c69f8d18c2529715b922df2fab
ozzy castillo <oswaldo.castillo@revature.net>
2021-08-20 16:19:28 -0500

    Completed

--- src/main/java/com.revature.challenge.problems/MainWordBucketChallenge.java
+++ src/main/java/com.revature.challenge.problems/MainWordBucketChallenge.java
@@ -7,6 +7,18 @@ public class MainWordBucketChallenge {
         String str = "she sells sea shells by the sea";
         WordBucket.bucketize(str,N);
 
+        N = 7;
+        String str2 = "the mouse jumped over the cheese";
+        WordBucket.bucketize(str2,N);
+
+        N=20;
+        String str3 = "fairy dust coated the air";
+        WordBucket.bucketize(str3,N);
+
+        N=2;
+        String str4 = "a b c d e";
+        WordBucket.bucketize(str4,N);
+
     }
 
 }
--- src/main/java/com.revature.challenge.problems/WordBucket.java
+++ src/main/java/com.revature.challenge.problems/WordBucket.java
@@ -6,44 +6,44 @@ import java.util.Arrays;
 public class WordBucket {
 
     public static void bucketize(String str, int N){
-        String[] strArray = str.split(" ");
-        ArrayList<String> wordsAdded = new ArrayList<>();
+        String[] words = str.split(" ");
+        ArrayList<String> addedWords = new ArrayList<>();
         ArrayList<String> bucket = new ArrayList<>();
 
-        for (int i = 0; i < strArray.length; i++){
-            if(!wordsAdded.contains(strArray[i])){
-                if(strArray[i].length()>N){
-                    wordsAdded.add(strArray[i]);
-                    bucket.add(strArray[i]);
-                }else{
-                    if(i < strArray.length -1){
-                        int length = 0;
-                        for (int j = i; j < strArray.length; j++) {
-                            if(j==i) {
-                                length += strArray[j].length();
-                            }else{
-                                length += strArray[j].length()+1;
-                            }
-
-                            if(length >= N){
-                                break;
-                            }else{
-                                wordsAdded.add(strArray[j]);
-                                bucket.add(strArray[i]);
-                            }
-                        }
-                    }else{
-                        wordsAdded.add(strArray[i]);
-                        bucket.add(strArray[i]);
-                    }
-
-                }
+        int length =0;
+        for(int i=0; i < words.length; i++){
+            if(i != 0 && i < words.length -1){
+                length += words[i].length()+1;
+            }else{
+                length += words[i].length();
             }
-
+            if(length <= N){
+                addedWords.add(words[i]);
+                if (i== words.length -1){
+                    addToBukect(words, addedWords, bucket, i);
+                    length = words[i].length();
+                }
+            }else{
+                if (i== words.length -1){
+                    addToBukect(words, addedWords, bucket, i);
+                }
+                addToBukect(words, addedWords, bucket, i);
+                length=0;
+                length = words[i].length()+1;
+            }
         }
-
         System.out.println(bucket.toString());
+    }
 
+    private static void addToBukect(String[] words, ArrayList<String> addedWords, ArrayList<String> bucket, int i) {
+        StringBuilder supportStr = new StringBuilder();
+        for(String word : addedWords){
+            supportStr.append(word);
+            supportStr.append(" ");
+        }
+        bucket.add(String.valueOf(supportStr).trim());
+        addedWords.clear();
+        addedWords.add(words[i]);
     }
 
 
